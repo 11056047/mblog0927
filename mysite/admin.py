9209e42595bebd9cbca5dcbb29df49cc85b68796
11056047 <11056047@ntub.edu.tw>
2023-10-04 16:00:03 +0800

    110560471004

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from mysite.models import Post
 # Register your models here.
+
+class PostAdaim(admin.ModelAdmin):
+    list_display=("title",'slug','pub_date')
+admin.site.register(Post,PostAdaim)
